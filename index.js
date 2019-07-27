6cadcf46c4cd6dba052ee393ad50c6c32479edcf
joelmanjet <joel.biju.joseph@gmail.com>
2019-07-27 09:14:38 -0400

    spelling correction

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const LaunchRequestHandler = {
     },
     handle(handlerInput) {
         //welcome message
-        let speechText = 'Welcome to my calculator you can add, subtract';
+        let speechText = 'Welcome to My Calculator. You can say, add 2 and 5, or multiply 4 and 8.';
         //welcome screen message
-        let displayText = "Welcome to my calculator you can add, subtract'"
+        let displayText = "Welcome to My Calculator"
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
@@ -25,78 +25,77 @@ const LaunchRequestHandler = {
 };
 
 //implement custom handlers
-const AddIntentHandeler = {
-  canHandel(handelerInput) {
-    return handelerInput.requestEnvelope.request.type == 'IntentRequest'
-    && handelerInput.requestEnvelope.request.intent.name == 'AddIntent'
-  },
-  handel(handelerInput) {
-    let speechText = '';
-    let displayText = '';
-    let intent = handelerInput.requestEnvelope.request.intent;
-    let firstNumber = intent.slots.firstNumber.value;
-    let secondNumber = intent.slots.secondNumber.value;
+const AddIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AddIntent'
+    },
+    handle(handlerInput) {
+        let speechText = '';
+        let displayText = '';
+        let intent = handlerInput.requestEnvelope.request.intent;
+        let firstNumber = intent.slots.firstNumber.value;
+        let secondNumber = intent.slots.secondNumber.value;
 
-    if ( firstNumber && secondNumber) {
-      //Preform operation
-      let result = parseInt(firstNumber) + parseInt(secondNumber);
-      speechText = `The result of ${firstNumber} plus ${secondNumber} is ${result}`;
-      displayText = `${result}`;
+        if (firstNumber && secondNumber) {
+            //Perform operation
+            let result = parseInt(firstNumber) + parseInt(secondNumber);
+            speechText = `The result of ${firstNumber} plus ${secondNumber} is ${result}`;
+            displayText = `${result}`;
 
-      return handelerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard(appName, displayText)
-      .withShouldEndSession(true)
-      .getResponse();
-
-
-    } else {
-      //Ask for the requierd input
-      return handelerInput.responseBuilder
-      .addDelegateDirective(intent)
-      .getResponse();
-
-    }
-
-  }
-};
-
-const SubtractIntentHandeler = {
-  canHandel(handelerInput) {
-    return handelerInput.requestEnvelope.request.type == 'IntentRequest'
-    && handelerInput.requestEnvelope.request.intent.name == 'SubtractIntent'
-  },
-  handel(handelerInput) {
-    let speechText = '';
-    let displayText = '';
-    let intent = handelerInput.requestEnvelope.request.intent;
-    let firstNumber = intent.slots.firstNumber.value;
-    let secondNumber = intent.slots.secondNumber.value;
-
-    if ( firstNumber && secondNumber) {
-      //Preform operation
-      let result = parseInt(secondNumber) - parseInt(firstNumber);
-      speechText = `The result of ${secondNumber} minus ${firstNumber} is ${result}`;
-      displayText = `${result}`;
-
-      return handelerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard(appName, displayText)
-      .withShouldEndSession(true)
-      .getResponse();
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .withSimpleCard(appName, displayText)
+                .withShouldEndSession(true)
+                .getResponse();
 
 
-    } else {
-      //Ask for the requierd input
-      return handelerInput.responseBuilder
-      .addDelegateDirective(intent)
-      .getResponse();
+        } else {
+            //Ask for the required input
+            return handlerInput.responseBuilder
+                .addDelegateDirective(intent)
+                .getResponse();
+
+        }
 
     }
-
-  }
 };
 
+const SubtractIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'SubtractIntent'
+    },
+    handle(handlerInput) {
+        let speechText = '';
+        let displayText = '';
+        let intent = handlerInput.requestEnvelope.request.intent;
+        let firstNumber = intent.slots.firstNumber.value;
+        let secondNumber = intent.slots.secondNumber.value;
+
+        if (firstNumber && secondNumber) {
+            //Perform operation
+            let result = parseInt(secondNumber) - parseInt(firstNumber);
+            speechText = `The result of ${secondNumber} minus ${firstNumber} is ${result}`;
+            displayText = `${result}`;
+
+            return handlerInput.responseBuilder
+                .speak(speechText)
+                .withSimpleCard(appName, displayText)
+                .withShouldEndSession(true)
+                .getResponse();
+
+
+        } else {
+            //Ask for the required input
+            return handlerInput.responseBuilder
+                .addDelegateDirective(intent)
+                .getResponse();
+
+        }
+
+    }
+};
 
 //end Custom handlers
 
@@ -107,7 +106,7 @@ const HelpIntentHandler = {
     },
     handle(handlerInput) {
         //help text for your skill
-        let speechText = 'You can add or subtract integers';
+        let speechText = 'You can say add 3 and 5 or divide 50 by 2';
 
         return handlerInput.responseBuilder
             .speak(speechText)
@@ -142,40 +141,12 @@ const SessionEndedRequestHandler = {
     }
 };
 
-const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
-
-    return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
-      .getResponse();
-  },
-};
-
 //Lambda handler function
 //Remember to add custom request handlers here
-let skill;
-exports.handler = async function (event, context) {
-  console.log(`REQUEST++++${JSON.stringify(event)}`);
-  if (!skill) {
-    skill = Alexa.SkillBuilders.custom()
-      .addRequestHandlers(
-        LaunchRequestHandler,
-        AddIntentHandeler,SubtractIntentHandeler,
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(LaunchRequestHandler,
+        AddIntentHandler,
+        SubtractIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-      )
-      .addErrorHandlers(ErrorHandler)
-      .create();
-  }
-
-  const response = await skill.invoke(event, context);
-  console.log(`RESPONSE++++${JSON.stringify(response)}`);
-
-  return response;
-};
+        SessionEndedRequestHandler).lambda();
